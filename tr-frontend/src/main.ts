fa5fdc56ed1cea2317f864db0e285f4088a2e6c7
borja <borja@fixu.cl>
2026-08-08 17:50:25 -0400

    initial commit

---
+++ tr-frontend/src/main.ts
@@ -0,0 +1,15 @@
+//import './style.css'
+
+//const texto: string = "Hola mundo";
+
+//document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//<div>${texto}</div>
+//`;
+
+const appContainer = document.getElementById('app');
+
+if(appContainer){
+  appContainer.innerHTML =`
+    <h1>Talento Ready</h1>
+  `;
+}
